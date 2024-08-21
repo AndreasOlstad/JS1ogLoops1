ad68df8e70329adaf6adb9a7943767f13772467f
AndreasOlstad <andreasolstad2007@gmail.com>
2024-08-21 12:29:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+//Oppgave 1
+/*
+const username = "Andreas";
+const password = "000";
+
+let myUsername = prompt("Skriv ditt navn");
+let myPassword = prompt("Skriv ditt passord");
+
+if (username == myUsername && password == myPassword) {
+  console.log("Velkommen til systemet");
+} else {
+  console.log("Du er ikke velkommen.");
+}
+*/
+
+//Oppgave 2
+/*
+let t1 = Number (prompt ("t1"))
+let t2 = Number (prompt ("t2"))
+let t3 = Number (prompt ("t3"))
+
+console.log ((t1+t2+t3)/3)
+*/
+
+//Oppgave 3
+/*
+let f = 86;
+let c = (f-32)*5/9;
+console.log(c)
+*/
+/*
+let c = 30;
+let f = c * (9/5)+32;
+*/
+
+//Oppgave 4
+/*
+const minVerdi = 4;
+let sum = 0;
+for (let i = 1; i < minVerdi+1; i++) {
+  console.log(i);
+  sum = sum + i;
+}
+console.log(sum);
+*/
+
+//Oppgave 5
+/*
+let poeng = 30;
+if (poeng <= 30) {
+    console.log('Karakter er 1')
+} else if (poeng <= 45) {
+    console.log('Karakter er 2')
+} else if (poeng <= 60) {
+    console.log('Karakter er 3')
+} else if (poeng <= 75) {
+    console.log('Karakter er 4')
+} else if (poeng <= 90) {
+    console.log('Karakter er 5')
+} else {
+    console.log('Karakter er 6')
+}
+*/
